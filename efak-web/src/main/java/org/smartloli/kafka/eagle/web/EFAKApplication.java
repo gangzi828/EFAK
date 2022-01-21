2aad086f71b55e1833d633971c5e6eebf19c63a3
gangzi <1139872666@qq.com>
2022-01-21 13:20:02 +0800

    添加了spring boot pom 依赖

---
+++ efak-web/src/main/java/org/smartloli/kafka/eagle/web/EFAKApplication.java
@@ -0,0 +1,13 @@
+package org.smartloli.kafka.eagle.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFAKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EFAKApplication.class, args);
+	}
+
+}
